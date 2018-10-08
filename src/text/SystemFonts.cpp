0fa2028dca9f9afb09a7f57379822537b105a8e6
Andrew Bell <cinder.afb@gmail.com>
2018-10-08 17:51:21 -0400

    Reformatting SystemFonts

--- src/text/SystemFonts.cpp
+++ src/text/SystemFonts.cpp
@@ -1,17 +1,6 @@
 #include "text/SystemFonts.h"
-#include "cinder/Font.h"
-
-//#if defined( cinder_msw_desktop )
-//#include <windows.h>
-//#define max(a, b) (((a) > (b)) ? (a) : (b))
-//#define min(a, b) (((a) < (b)) ? (a) : (b))
-//#include <gdiplus.h>
-//#undef min
-//#undef max
-//#include "cinder/msw/cindermsw.h"
-//#include "cinder/msw/cindermswgdiplus.h"
-//#pragma comment(lib, "gdiplus")
-//#endif
+#include "cinder/Unicode.h"
+#include "cinder/app/App.h"
 
 #if defined( CINDER_MSW_DESKTOP )
 	#include "cinder/msw/cindermsw.h"
@@ -21,181 +10,177 @@
 	#include <CoreText/CoreText.h>
 #endif
 
-#include "cinder/Unicode.h"
-#include "cinder/app/App.h"
-
 #include <unordered_map>
 
+namespace text {
 
-namespace text
-{
-
-	// Windows
+// Windows
 #if defined( CINDER_MSW_DESKTOP )
-	HDC mFontDC = nullptr;
+HDC mFontDC = nullptr;
 
-	SystemFonts::SystemFonts()
-	{
-		mFontDC = ::CreateCompatibleDC( NULL );
+SystemFonts::SystemFonts()
+{
+	mFontDC = ::CreateCompatibleDC( NULL );
 
-		mDefaultFamily = "Arial";
-		mDefaultStyle = "Regular";
-		mDefaultSize = 12;
+	mDefaultFamily = "Arial";
+	mDefaultStyle = "Regular";
+	mDefaultSize = 12;
+}
+
+
+int CALLBACK EnumFacesExProc( ENUMLOGFONTEX* lpelfe, NEWTEXTMETRICEX* lpntme, int FontType, LPARAM lParam )
+{
+	std::string familyName = ci::toUtf8( ( char16_t* )lpelfe->elfLogFont.lfFaceName );
+	std::string style = ci::toUtf8( ( char16_t* )lpelfe->elfStyle );
+
+	( *reinterpret_cast<std::map<std::string, std::vector<std::string>>*>( lParam ) )[familyName].push_back( style );
+
+	return 1;
+}
+
+struct HFontRequest {
+	LOGFONT logfont;
+	std::string family;
+	std::string style;
+	bool fontFound = false;
+};
+
+HFontRequest mHFontRequest;
+
+int CALLBACK LoadHFontExProc( ENUMLOGFONTEX* lpelfe, NEWTEXTMETRICEX* lpntme, int FontType, LPARAM lParam )
+{
+	HFontRequest* request = reinterpret_cast<HFontRequest*>( lParam );
+
+	std::string familyName = ci::toUtf8( ( char16_t* )lpelfe->elfLogFont.lfFaceName );
+	std::transform( familyName.begin(), familyName.end(), familyName.begin(), ::tolower );
+
+	std::string style = ci::toUtf8( ( char16_t* )lpelfe->elfStyle );
+	std::transform( style .begin(), style.end(), style.begin(), ::tolower );
+
+	if( familyName == request->family && style == request->style ) {
+		request->logfont = lpelfe->elfLogFont;
+		request->fontFound = true;
 	}
 
+	return 1;
+}
 
-	int CALLBACK EnumFacesExProc( ENUMLOGFONTEX* lpelfe, NEWTEXTMETRICEX* lpntme, int FontType, LPARAM lParam )
-	{
-		std::string familyName = ci::toUtf8( ( char16_t* )lpelfe->elfLogFont.lfFaceName );
-		std::string style = ci::toUtf8( ( char16_t* )lpelfe->elfStyle );
+void SystemFonts::listFaces()
+{
+	mFaces.clear();
 
-		( *reinterpret_cast<std::map<std::string, std::vector<std::string>>*>( lParam ) )[familyName].push_back( style );
+	::LOGFONT lf;
+	lf.lfCharSet = ANSI_CHARSET;
+	lf.lfFaceName[0] = '\0';
 
-		return 1;
-	}
+	::EnumFontFamiliesEx( mFontDC, &lf, ( FONTENUMPROC )EnumFacesExProc, reinterpret_cast<LPARAM>( &mFaces ), 0 );
 
-	struct HFontRequest {
-		LOGFONT logfont;
-		std::string family;
-		std::string style;
-		bool fontFound = false;
-	};
+	for( auto& family : mFaces ) {
+		ci::app::console() << family.first << std::endl;
+		ci::app::console() << "---------------------" << std::endl;
 
-	HFontRequest mHFontRequest;
-
-	int CALLBACK LoadHFontExProc( ENUMLOGFONTEX* lpelfe, NEWTEXTMETRICEX* lpntme, int FontType, LPARAM lParam )
-	{
-		HFontRequest* request = reinterpret_cast<HFontRequest*>( lParam );
-
-		std::string familyName = ci::toUtf8( ( char16_t* )lpelfe->elfLogFont.lfFaceName );
-		std::transform( familyName.begin(), familyName.end(), familyName.begin(), ::tolower );
-
-		std::string style = ci::toUtf8( ( char16_t* )lpelfe->elfStyle );
-		std::transform( style .begin(), style.end(), style.begin(), ::tolower );
-
-		if( familyName == request->family && style == request->style ) {
-			request->logfont = lpelfe->elfLogFont;
-			request->fontFound = true;
+		for( auto& style : family.second ) {
+			ci::app::console() << style << std::endl;
 		}
 
-		return 1;
+		ci::app::console() << std::endl;
 	}
+}
 
-	void SystemFonts::listFaces()
-	{
-		mFaces.clear();
+ci::BufferRef SystemFonts::getFontBuffer( std::string family, std::string style )
+{
+	::LOGFONT lf;
+	lf.lfCharSet = ANSI_CHARSET;
+	lf.lfFaceName[0] = '\0';
 
-		::LOGFONT lf;
-		lf.lfCharSet = ANSI_CHARSET;
-		lf.lfFaceName[0] = '\0';
+	std::u16string faceName = ci::toUtf16( family );
+	::StringCchCopy( lf.lfFaceName, LF_FACESIZE, ( LPCTSTR )faceName.c_str() );
 
-		::EnumFontFamiliesEx( mFontDC, &lf, ( FONTENUMPROC )EnumFacesExProc, reinterpret_cast<LPARAM>( &mFaces ), 0 );
+	mHFontRequest.family = family;
+	mHFontRequest.style = style;
 
-		for( auto& family : mFaces ) {
-			ci::app::console() << family.first << std::endl;
-			ci::app::console() << "---------------------" << std::endl;
+	::EnumFontFamiliesEx( mFontDC, &lf, ( FONTENUMPROC )LoadHFontExProc, reinterpret_cast<LPARAM>( &mHFontRequest ), 0 );
 
-			for( auto& style : family.second ) {
-				ci::app::console() << style << std::endl;
-			}
+	HFONT hFont;
 
-			ci::app::console() << std::endl;
-		}
-	}
+	if( mHFontRequest.fontFound ) {
+		hFont = ::CreateFontIndirectW( &mHFontRequest.logfont );
 
-	ci::BufferRef SystemFonts::getFontBuffer( std::string family, std::string style )
-	{
-		::LOGFONT lf;
-		lf.lfCharSet = ANSI_CHARSET;
-		lf.lfFaceName[0] = '\0';
+		if( hFont ) {
+			::SelectObject( mFontDC, hFont );
 
-		std::u16string faceName = ci::toUtf16( family );
-		::StringCchCopy( lf.lfFaceName, LF_FACESIZE, ( LPCTSTR )faceName.c_str() );
+			DWORD fontSize = ::GetFontData( mFontDC, 0, 0, NULL, 0 );
 
-		mHFontRequest.family = family;
-		mHFontRequest.style = style;
-
-		::EnumFontFamiliesEx( mFontDC, &lf, ( FONTENUMPROC )LoadHFontExProc, reinterpret_cast<LPARAM>( &mHFontRequest ), 0 );
-
-		HFONT hFont;
-
-		if( mHFontRequest.fontFound ) {
-			hFont = ::CreateFontIndirectW( &mHFontRequest.logfont );
-
-			if( hFont ) {
-				::SelectObject( mFontDC, hFont );
-
-				DWORD fontSize = ::GetFontData( mFontDC, 0, 0, NULL, 0 );
-
-				void* fontBuffer = new BYTE[fontSize];
-				DWORD length = ::GetFontData( mFontDC, 0, 0, fontBuffer, fontSize );
+			void* fontBuffer = new BYTE[fontSize];
+			DWORD length = ::GetFontData( mFontDC, 0, 0, fontBuffer, fontSize );
 
 
-				return ci::Buffer::create( fontBuffer, ( unsigned int )length );
-			}
-			else {
-				return nullptr;
-			}
+			return ci::Buffer::create( fontBuffer, ( unsigned int )length );
 		}
 		else {
 			return nullptr;
 		}
 	}
-
-#else
-	SystemFonts::SystemFonts()
-	{
-		mDefaultFamily = "ArialMT";
-		mDefaultStyle = "Regular";
-		mDefaultSize = 12;
-		
-		CFArrayRef fontUrls = ::CTFontManagerCopyAvailableFontURLs();
-		CFIndex count = ::CFArrayGetCount( fontUrls );
-		
-		const size_t anchorPrefixLength = strlen( "postscript-name=" ); 
-		for( CFIndex i = 0; i < count; ++i ) {
-			unsigned char bytes[4096];
-			char path[4096], postscriptName[4096];
-			CFRange pathRange, pathRangeIncludingSeparators, psNameRange, psNameRangeIncludingSeparators;
-			
-			CFURLRef fontUrl = (CFURLRef)::CFArrayGetValueAtIndex( fontUrls, i );
-			pathRange = ::CFURLGetByteRangeForComponent( fontUrl, kCFURLComponentPath, &pathRangeIncludingSeparators );
-			psNameRange = ::CFURLGetByteRangeForComponent( fontUrl, kCFURLComponentFragment, &psNameRangeIncludingSeparators );
-			if( pathRange.location != kCFNotFound && psNameRange.location != kCFNotFound && psNameRange.length > anchorPrefixLength ) {
-				::CFURLGetBytes( fontUrl, bytes, sizeof(bytes) );
-				
-				CFStringRef pathCopy = ::CFStringCreateWithSubstring( kCFAllocatorDefault, ::CFURLGetString( fontUrl ), pathRange );
-				CFStringRef unescapedPath = ::CFURLCreateStringByReplacingPercentEscapes( kCFAllocatorDefault, pathCopy, CFSTR("") );
-				::CFStringGetCString( unescapedPath, path, 4096, kCFStringEncodingUTF8 );
-				::CFRelease( pathCopy );
-				::CFRelease( unescapedPath );				
-				
-				psNameRange.length -= anchorPrefixLength;
-				psNameRange.location += anchorPrefixLength;
-				strncpy( postscriptName, (const char *)&bytes[psNameRange.location], psNameRange.length );
-				postscriptName[psNameRange.length] = 0;
-			}
-			else
-				continue;
-			
-			mSystemNameToPath[std::string(postscriptName)] = ci::fs::path( path );
-		}
-		::CFRelease( fontUrls );
+	else {
+		return nullptr;
 	}
-
-	void SystemFonts::listFaces()
-	{
-	}
-
-	ci::BufferRef SystemFonts::getFontBuffer( std::string family, std::string style )
-	{
-		return std::make_shared<ci::Buffer>( ci::loadFile( mSystemNameToPath["ArialMT"] ) );
-		
-		if( mSystemNameToPath.find( family ) != mSystemNameToPath.end() ) 
-			return std::make_shared<ci::Buffer>( ci::loadFile( mSystemNameToPath[family] ) );
-		else
-			return ci::BufferRef();
-	}
-
-#endif
 }
+
+#elif defined( CINDER_COCOA )
+
+SystemFonts::SystemFonts()
+{
+	mDefaultFamily = "ArialMT";
+	mDefaultStyle = "Regular";
+	mDefaultSize = 12;
+	
+	CFArrayRef fontUrls = ::CTFontManagerCopyAvailableFontURLs();
+	CFIndex count = ::CFArrayGetCount( fontUrls );
+	
+	const size_t anchorPrefixLength = strlen( "postscript-name=" ); 
+	for( CFIndex i = 0; i < count; ++i ) {
+		unsigned char bytes[4096];
+		char path[4096], postscriptName[4096];
+		CFRange pathRange, pathRangeIncludingSeparators, psNameRange, psNameRangeIncludingSeparators;
+		
+		CFURLRef fontUrl = (CFURLRef)::CFArrayGetValueAtIndex( fontUrls, i );
+		pathRange = ::CFURLGetByteRangeForComponent( fontUrl, kCFURLComponentPath, &pathRangeIncludingSeparators );
+		psNameRange = ::CFURLGetByteRangeForComponent( fontUrl, kCFURLComponentFragment, &psNameRangeIncludingSeparators );
+		if( pathRange.location != kCFNotFound && psNameRange.location != kCFNotFound && psNameRange.length > anchorPrefixLength ) {
+			::CFURLGetBytes( fontUrl, bytes, sizeof(bytes) );
+			
+			CFStringRef pathCopy = ::CFStringCreateWithSubstring( kCFAllocatorDefault, ::CFURLGetString( fontUrl ), pathRange );
+			CFStringRef unescapedPath = ::CFURLCreateStringByReplacingPercentEscapes( kCFAllocatorDefault, pathCopy, CFSTR("") );
+			::CFStringGetCString( unescapedPath, path, 4096, kCFStringEncodingUTF8 );
+			::CFRelease( pathCopy );
+			::CFRelease( unescapedPath );				
+			
+			psNameRange.length -= anchorPrefixLength;
+			psNameRange.location += anchorPrefixLength;
+			strncpy( postscriptName, (const char *)&bytes[psNameRange.location], psNameRange.length );
+			postscriptName[psNameRange.length] = 0;
+		}
+		else
+			continue;
+		
+		mSystemNameToPath[std::string(postscriptName)] = ci::fs::path( path );
+	}
+	::CFRelease( fontUrls );
+}
+
+void SystemFonts::listFaces()
+{
+}
+
+ci::BufferRef SystemFonts::getFontBuffer( std::string family, std::string style )
+{
+	return std::make_shared<ci::Buffer>( ci::loadFile( mSystemNameToPath["ArialMT"] ) );
+	
+	if( mSystemNameToPath.find( family ) != mSystemNameToPath.end() ) 
+		return std::make_shared<ci::Buffer>( ci::loadFile( mSystemNameToPath[family] ) );
+	else
+		return ci::BufferRef();
+}
+#endif // defined( CINDER_COCOA )
+
+} // namespace text
--- src/text/SystemFonts.h
+++ src/text/SystemFonts.h
@@ -1,40 +1,40 @@
 #pragma once
 
+#include "cinder/Buffer.h"
+#include "text/Font.h"
+
 #include <memory>
 #include <map>
 
-#include "cinder/Buffer.h"
+namespace text {
 
-#include "text/Font.h"
+typedef std::shared_ptr<class SystemFonts> SystemFontsRef;
 
-namespace text
+class SystemFonts
 {
-	typedef std::shared_ptr<class SystemFonts> SystemFontsRef;
-
-	class SystemFonts
+  public:
+	static SystemFontsRef get()
 	{
-		public:
-			static SystemFontsRef get()
-			{
-				static SystemFontsRef ref( new SystemFonts() );
-				return ref;
-			};
-
-			ci::BufferRef getFontBuffer( std::string family, std::string style );
-
-			std::string getDefaultFamily() { return mDefaultFamily; };
-			std::string getDefaultStyle() { return mDefaultStyle; };
-			int getDefaultSize() { return mDefaultSize; };
-
-		private:
-			SystemFonts();
-
-			void listFaces();
-			std::map<std::string,std::vector<std::string>> 	mFaces;
-			std::map<std::string,ci::fs::path>				mSystemNameToPath;
-
-			std::string mDefaultFamily;
-			std::string mDefaultStyle;
-			int mDefaultSize;
+		static SystemFontsRef ref( new SystemFonts() );
+		return ref;
 	};
-}
+
+	ci::BufferRef getFontBuffer( std::string family, std::string style );
+
+	std::string getDefaultFamily() { return mDefaultFamily; };
+	std::string getDefaultStyle() { return mDefaultStyle; };
+	int getDefaultSize() { return mDefaultSize; };
+
+  private:
+	SystemFonts();
+
+	void listFaces();
+	std::map<std::string,std::vector<std::string>> 	mFaces;
+	std::map<std::string,ci::fs::path>				mSystemNameToPath;
+
+	std::string	mDefaultFamily;
+	std::string	mDefaultStyle;
+	int			mDefaultSize;
+};
+
+} // namespace text
