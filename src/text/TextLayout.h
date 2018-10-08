02370f019e1ac311538455447a8abeac066ac624
Andrew Bell <cinder.afb@gmail.com>
2018-10-08 17:55:10 -0400

    Reformatting TextLayout

--- src/text/TextLayout.h
+++ src/text/TextLayout.h
@@ -12,165 +12,165 @@
 #include "text/TextUnits.h"
 #include "text/Shaper.h"
 
-namespace text
-{
-	typedef enum Alignment { LEFT, CENTER, RIGHT, JUSTIFIED } Alignment;
-	enum { GROW = 0 };
+namespace text {
 
-	class Layout
-	{
-		public:
-			// A single character
-			typedef struct {
-				uint32_t index;
-				ci::Rectf bbox;
-				unsigned int top;
-				std::string value;
-			} Glyph;
+typedef enum Alignment { LEFT, CENTER, RIGHT, JUSTIFIED } Alignment;
+enum { GROW = 0 };
 
-			// A group of characters with the same attributes
-			struct Run {
-				Run( const Font& font, const ci::Color& color, const float& opacity )
-					: font( font )
-					, color( color )
-					, opacity( opacity )
-				{};
+class Layout {
+  public:
+	// A single character
+	typedef struct {
+		uint32_t index;
+		ci::Rectf bbox;
+		unsigned int top;
+		std::string value;
+	} Glyph;
 
-				Font font;
-				ci::Color color;
-				float opacity;
+	// A group of characters with the same attributes
+	struct Run {
+		Run( const Font& font, const ci::Color& color, const float& opacity )
+			: font( font )
+			, color( color )
+			, opacity( opacity )
+		{};
 
-				std::vector<Glyph> glyphs;
-			};
+		Font font;
+		ci::Color color;
+		float opacity;
 
-			// A line of runs fit within the layout
-			struct Line {
-				std::vector<Run> runs;
-				int width;
-				int getTotalGlyphs()
-				{
-					int totalGlyphs = 0;
+		std::vector<Glyph> glyphs;
+	};
 
-					for( auto& run : runs ) {
-						totalGlyphs += run.glyphs.size();
-					}
+	// A line of runs fit within the layout
+	struct Line {
+		std::vector<Run> runs;
+		int width;
+		int getTotalGlyphs()
+		{
+			int totalGlyphs = 0;
 
-					return totalGlyphs;
-				}
-
-			};
-
-			Layout();
-
-			// Layout Calculation
-			void calculateLayout( std::string text );
-			void calculateLayout( const AttributedString& attrString );
-
-			// Lines
-			const std::vector<Line>& getLines() const { return mLines; };
-			std::vector<Line>& getLines() { return mLines; };
-
-			// Layout Attributes
-			const Font& getFont() const { return mFont; }
-			Layout& setFont( const Font& font ) { mFont = font; return *this; }
-
-			const ci::Color& getColor() const { return mColor; }
-			Layout& setColor( const ci::Color& color ) { mColor = color; return *this; }
-
-			const ci::vec2& getSize() const { return mSize; }
-			Layout& setSize( ci::vec2 size ) { mSize = size; return *this; }
-
-			const ci::vec2 measure();
-
-			const std::vector<ci::Rectf>& getGlyphBoxes() const { return mGlyphBoxes; }
-
-			float getLineHeight() const { return mLineHeight.getValue( getFont().getSize() ); }
-			Layout& setLineHeight( const float& lineHeight ) { mLineHeight = text::Unit( lineHeight ); return *this; };
-			Layout& setLineHeight( const Unit& lineHeight ) { mLineHeight = lineHeight; return *this; };
-
-			float getTracking() const { return mTracking.getValue( getFont().getSize() ); }
-			Layout& setTracking( float tracking ) { mTracking = text::Unit( tracking ); return *this; };
-			Layout& setTracking( const Unit& tracking ) { mTracking = tracking; return *this; };
-
-			Layout& setUseLigatures( const bool useLigatures ) { mUseLigatures = useLigatures; return *this; };
-			Layout& setUseKerning( const bool useKerning ) { mUseKerning = useKerning; return *this; };
-			Layout& setUseClig( const bool useClig ) { mUseClig = useClig; return *this; };
-			Layout& setUseCalt( const bool useCalt ) { mUseCalt = useCalt; return *this; };
-
-			std::string getLanguage() const { return mLanguage; }
-			Layout& setLanguage( std::string language ) { mLanguage = language; return *this; }
-
-			hb_script_t getScript() const { return mScript; }
-			Layout& setScript( hb_script_t script ) { mScript = script; return *this; }
-
-			hb_direction_t getDirection() const { return mDirection; }
-			Layout& setDirection( hb_direction_t direction )
-			{
-				mDirection = direction;
-
-				// If alignment isn't manually set, default to LEFT for LTR and RIGHT for RTL
-				if( mUseDefaultAlignment ) {
-					mAlignment = mDirection == HB_DIRECTION_LTR ? Alignment::LEFT : Alignment::RIGHT;
-				}
-
-				return *this;
+			for( auto& run : runs ) {
+				totalGlyphs += run.glyphs.size();
 			}
 
-			const Alignment& getAlignment() const { return mAlignment; }
-			Layout& setAlignment( Alignment alignment )
-			{
-				mAlignment = alignment;
-				mUseDefaultAlignment = false;
-				return *this;
-			};
+			return totalGlyphs;
+		}
 
-		private:
-			// Attributes
-			Font mFont;
-			ci::Color mColor;
-			Alignment mAlignment;
-			bool mUseDefaultAlignment;
-			text::Unit mLineHeight;
-			text::Unit mTracking;
-
-			bool mUseLigatures;
-			bool mUseKerning;
-			bool mUseClig;
-			bool mUseCalt;
-
-			std::string mLanguage;
-			hb_script_t mScript;
-			hb_direction_t mDirection;
-
-			ci::vec2 mSize;
-			ci::vec2 mLayoutSize;
-
-			// Layout calculation
-			void resetLayout();
-
-			// Line breaking
-			struct BreakIndices {
-				int textBreakIndex = -1;
-				int glyphBreakIndex = -1;
-				bool found = false;
-			};
-			BreakIndices getClosestBreakForShapedText( int startIndex, const std::vector<Shaper::Glyph>& shapedGlyphs, const std::vector<uint8_t> lineBreaks, const hb_direction_t& direction );
-
-			ci::vec2 mCurDirection;
-			float mCharPos, mLinePos;
-			Line mCurLine;
-			float mCurLineWidth = 0;
-			float mCurLineHeight = 0;
-			std::vector<ci::Rectf> mGlyphBoxes;
-
-			float getLineHeightForSubstring( const AttributedString::Substring& substring, const Font& runFont );
-
-			void addSubstringToCurLine( AttributedString::Substring& substring );
-			void addRunToCurLine( Run& run );
-			void addCurLine();
-			void applyAlignment();
-			std::vector<Line> mLines;
-
-			bool mMaxLinesReached = false;
 	};
-}
+
+	Layout();
+
+	// Layout Calculation
+	void calculateLayout( std::string text );
+	void calculateLayout( const AttributedString& attrString );
+
+	// Lines
+	const std::vector<Line>& getLines() const { return mLines; };
+	std::vector<Line>& getLines() { return mLines; };
+
+	// Layout Attributes
+	const Font& getFont() const { return mFont; }
+	Layout& setFont( const Font& font ) { mFont = font; return *this; }
+
+	const ci::Color& getColor() const { return mColor; }
+	Layout& setColor( const ci::Color& color ) { mColor = color; return *this; }
+
+	const ci::vec2& getSize() const { return mSize; }
+	Layout& setSize( ci::vec2 size ) { mSize = size; return *this; }
+
+	const ci::vec2 measure();
+
+	const std::vector<ci::Rectf>& getGlyphBoxes() const { return mGlyphBoxes; }
+
+	float getLineHeight() const { return mLineHeight.getValue( getFont().getSize() ); }
+	Layout& setLineHeight( const float& lineHeight ) { mLineHeight = text::Unit( lineHeight ); return *this; };
+	Layout& setLineHeight( const Unit& lineHeight ) { mLineHeight = lineHeight; return *this; };
+
+	float getTracking() const { return mTracking.getValue( getFont().getSize() ); }
+	Layout& setTracking( float tracking ) { mTracking = text::Unit( tracking ); return *this; };
+	Layout& setTracking( const Unit& tracking ) { mTracking = tracking; return *this; };
+
+	Layout& setUseLigatures( const bool useLigatures ) { mUseLigatures = useLigatures; return *this; };
+	Layout& setUseKerning( const bool useKerning ) { mUseKerning = useKerning; return *this; };
+	Layout& setUseClig( const bool useClig ) { mUseClig = useClig; return *this; };
+	Layout& setUseCalt( const bool useCalt ) { mUseCalt = useCalt; return *this; };
+
+	std::string getLanguage() const { return mLanguage; }
+	Layout& setLanguage( std::string language ) { mLanguage = language; return *this; }
+
+	hb_script_t getScript() const { return mScript; }
+	Layout& setScript( hb_script_t script ) { mScript = script; return *this; }
+
+	hb_direction_t getDirection() const { return mDirection; }
+	Layout& setDirection( hb_direction_t direction )
+	{
+		mDirection = direction;
+
+		// If alignment isn't manually set, default to LEFT for LTR and RIGHT for RTL
+		if( mUseDefaultAlignment ) {
+			mAlignment = mDirection == HB_DIRECTION_LTR ? Alignment::LEFT : Alignment::RIGHT;
+		}
+
+		return *this;
+	}
+
+	const Alignment& getAlignment() const { return mAlignment; }
+	Layout& setAlignment( Alignment alignment )
+	{
+		mAlignment = alignment;
+		mUseDefaultAlignment = false;
+		return *this;
+	};
+
+private:
+	// Attributes
+	Font mFont;
+	ci::Color mColor;
+	Alignment mAlignment;
+	bool mUseDefaultAlignment;
+	text::Unit mLineHeight;
+	text::Unit mTracking;
+
+	bool mUseLigatures;
+	bool mUseKerning;
+	bool mUseClig;
+	bool mUseCalt;
+
+	std::string mLanguage;
+	hb_script_t mScript;
+	hb_direction_t mDirection;
+
+	ci::vec2 mSize;
+	ci::vec2 mLayoutSize;
+
+	// Layout calculation
+	void resetLayout();
+
+	// Line breaking
+	struct BreakIndices {
+		int textBreakIndex = -1;
+		int glyphBreakIndex = -1;
+		bool found = false;
+	};
+	BreakIndices getClosestBreakForShapedText( int startIndex, const std::vector<Shaper::Glyph>& shapedGlyphs, const std::vector<uint8_t> lineBreaks, const hb_direction_t& direction );
+
+	ci::vec2 mCurDirection;
+	float mCharPos, mLinePos;
+	Line mCurLine;
+	float mCurLineWidth = 0;
+	float mCurLineHeight = 0;
+	std::vector<ci::Rectf> mGlyphBoxes;
+
+	float getLineHeightForSubstring( const AttributedString::Substring& substring, const Font& runFont );
+
+	void addSubstringToCurLine( AttributedString::Substring& substring );
+	void addRunToCurLine( Run& run );
+	void addCurLine();
+	void applyAlignment();
+	std::vector<Line> mLines;
+
+	bool mMaxLinesReached = false;
+};
+
+} // namespace text
