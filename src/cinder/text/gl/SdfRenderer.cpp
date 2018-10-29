8b60d8c68edd60382d50b1dbf6646e2227fcfa5f
Greg Kepler <gkepler@gmail.com>
2018-10-29 14:57:29 -0400

    Starting to stub out SdfRenderer classses.g

--- src/cinder/text/gl/SdfRenderer.cpp
+++ src/cinder/text/gl/SdfRenderer.cpp
@@ -1,9 +1,12 @@
 #include "SdfRenderer.h"
 
+#include "cinder/app/App.h"
 #include "cinder/gl/gl.h"
+#include "msdfgen/msdfgen.h"
+#include "msdfgen/util.h"0
 
 using namespace ci;
-//using namespace ci::app;
+using namespace ci::app;
 using namespace std;
 
 namespace cinder { namespace text { namespace gl {
@@ -16,5 +19,38 @@ SdfRenderer::SdfRenderer()
 SdfRenderer::~SdfRenderer()
 {
 }
+
+/*void SdfRenderer::setFontCacheFile( const fs::path& filePath )
+{
+	// set the font cache fle location
+}*/
+
+void SdfRenderer::loadFont( const Font& font, bool loadEntireFont )
+{
+	// look for font in font cache filre
+	// if font is found, load it from file
+	// else
+
+}
+
+// Cache glyphs to gl texture array(s)
+void SdfRenderer::cacheFont( const Font& font,  bool cacheEntireFont )
+{
+	if( cacheEntireFont ) {
+		// entire font
+		std::vector<uint32_t> glyphIndices = cinder::text::FontManager::get()->getGlyphIndices( font );
+		//cacheGlyphs( font, glyphIndices );
+	}
+	else {
+		// partial font
+		//cacheGlyphs( font, defaultUnicodeRange() );
+	}
+}
+/*
+void SdfRenderer::cacheGlyphs(  const Font& font, const std::vector<uint32_t> &glyphIndices )
+{
+	
+	
+}*/
 
 } } } // namespace cinder::text::gl
--- src/cinder/text/gl/SdfRenderer.h
+++ src/cinder/text/gl/SdfRenderer.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "cinder/text/FontManager.h"
 #include "cinder/gl/Texture.h"
 
 namespace cinder { namespace text { namespace gl {
@@ -9,6 +10,12 @@ class SdfRenderer
 public:
 	SdfRenderer();
 	~SdfRenderer();
+
+	//static void setFontCacheFile( const fs::path& filePath );
+	static void loadFont( const Font& font, bool loadEntireFont = false );
+
+private:
+	static void cacheFont( const Font& font, bool cacheEntireFont = false );
 };
 
 } } } // namespace cinder::text::gl
